b58d40cd6ecb95a0f5aff51c231fa51dd6961b04
Anshad Vattapoyil <anshad.musafir@gmail.com>
2017-09-05 16:15:33 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container" id="app-container">
+        <b-tabs pills>
+            <!-- Form -->
+            <b-tab title="Form" active>
+                <p>Form Content</p>
+            </b-tab>
+            <!-- List -->
+            <b-tab title="List" >
+                <p>List Content</p>
+            </b-tab>
+            <!-- Report -->
+            <b-tab title="Report">
+                <p>Report Content</p>
+            </b-tab>
+            <!-- Export -->
+            <b-tab title="Export">
+                <p>Export Content</p>
+            </b-tab>
+        </b-tabs>
+    </div>
+</template>
+<script src="./app.js"></script>
+<style lang="scss" src="./app.scss"></style>
---
+++ src/app.js
@@ -0,0 +1,14 @@
+/*
+* @Author: anshad
+* @Date:   2017-09-05 11:18:14
+* @Last Modified by:   anshad
+* @Last Modified time: 2017-09-05 11:18:17
+*/
+export default {
+	name: 'app',
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	}
+}
